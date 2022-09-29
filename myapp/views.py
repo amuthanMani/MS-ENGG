3f778465aaf589380ea5021de38dc8182f5895d1
Global Girl <globalgirl123@gmail.com>
2022-09-29 21:46:45 +0530

    final

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
-def index(requests): 
-   return HttpResponse(f'[myapp/Home.html, Products.html]')
-  
+def index(request): 
+   return render(request, 'myapp/Home.html')
+
