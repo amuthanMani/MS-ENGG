6e1cd9f46e54a383a3bcf546a385ee97611154ba
Global Girl <globalgirl123@gmail.com>
2022-09-30 20:56:19 +0530

    final

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-import static
-from Project1.settings import settings
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('Products.html', views.index1, name='index1'),
     path('Solutions.html', views.index2, name='index2'),
     path('Contacts.html', views.index3, name='index3'),
     path('Enquiry.html', views.index4, name='index4'),
+]
 
-] 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
