2e851b6107e75b0977c86018bd8d31c32fbd4e30
unknown <amuthanmani108@yahoo.com>
2022-10-09 19:38:29 +0530

    ini

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('Home.html', views.index1, name='index'),
     path('Products.html', views.index1, name='index1'),
     path('Solutions.html', views.index2, name='index2'),
     path('Contacts.html', views.index3, name='index3'),
