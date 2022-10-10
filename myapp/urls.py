12c68c7ebc394664ff004329f62e8d2e64da4cde
unknown <amuthanmani108@yahoo.com>
2022-10-10 10:56:04 +0530

    edit

--- myapp/urls.py
+++ myapp/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('Solutions.html', views.index2, name='index2'),
     path('Contacts.html', views.index3, name='index3'),
     path('Enquiry.html', views.index4, name='index4'),
+
 ] 
 
 urlpatterns += staticfiles_urlpatterns()
