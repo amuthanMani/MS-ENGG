9f3272c2f565dfbe6536ba1400af62eea04eb1f7
unknown <amuthanmani108@yahoo.com>
2022-10-19 08:51:20 +0530

    edit

--- Project1/settings.py
+++ Project1/settings.py
@@ -148,13 +148,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'images')
 
 
 =======
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
->>>>>>> cd74440fab1d5b3ad28cb4b3a79d40dd15dbd1ce
 
-STATICFILES_DIRS= [
-    os.path.join(BASE_DIR, "myapp"),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
